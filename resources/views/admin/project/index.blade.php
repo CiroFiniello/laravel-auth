9f0ed442e644b5874d3b970b943100fb5183d517
CodeKiro <kirofiniello@outlook.it>
2024-08-01 23:34:20 +0200

    ho salvato la mia utenza all'interno di un'altro seeder admin, per evitare di dovermi riregistrare ogni volta, poi ho chiamato il seed nel database, aggiungendolo alla call , infine l'ho testato facendo un seed, poi ho aggiunto il faker al project seeder, e creato un foreach in una tabella per visualizzare i risultati in pagina

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -1,2 +1,34 @@
 
 
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+                <table class="table table-dark table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                        <tr>
+                            <td>{{ $project->id }}</td>
+                            <td>{{ $project->title }}</td>
+                            <td>{{ $project->author }}</td>
+                            <td>{{ $project->date}}</td>
+                            <td><a href="{{route('admin.project.show', $project)}}" class="btn btn-primary btn-sm">Show</a></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+        </div>
+    </div>
+</div>
+@endsection
